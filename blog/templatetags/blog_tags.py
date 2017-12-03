fd022e7b14d3eb2df9189b355f112c929db750ca
shd <1396538376@qq.com>
2017-12-03 22:57:19 +0800

    update some features like RSS and markdown toc on 2017-12-03

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
-from ..models import Post, Category
+from django.db.models.aggregates import Count
+from ..models import Post, Category, Tag
 
 register = template.Library()
 
@@ -24,4 +25,12 @@ def archives():
 @register.simple_tag
 def get_categories():
     # 别忘了在顶部引入 Category 类
-    return Category.objects.all()
+    # 记得在顶部引入 count 函数
+    # Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+# 标签云
+@register.simple_tag
+def get_tags():
+    # 记得在顶部引入 Tag model
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
